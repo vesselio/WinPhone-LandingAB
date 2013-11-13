8528c86cae2ab0afcbf5628df06b3b72098a00a1
Kirill <kirill@vessel.io>
2013-11-13 21:28:01 +0200

    win phone ab demo

---
+++ LandingDemo/LandingDemo/HomePage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using Vessel;
+
+namespace LandingDemo
+{
+    public partial class HomePage : PhoneApplicationPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+
+            var v = VesselAB.whichVariation(VesselAB.whichTest());
+            if (v == TestVariation.A)
+            {
+                VariantB.Visibility = Visibility.Collapsed;
+                VariantA.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                VariantA.Visibility = Visibility.Collapsed;
+                VariantB.Visibility = Visibility.Visible;
+            }
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            VesselAB.startSession("Sign up screen");
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            VesselAB.endSession("Sign up screen");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            VesselAB.checkPointVisited("signedUpWithFacebook");
+        }
+
+    }
+}
---
+++ LandingDemo/LandingDemo/SplashPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Vessel;
+using System.Windows.Navigation;
+
+namespace LandingDemo
+{
+    public partial class SplashPage : PhoneApplicationPage
+    {
+        // Constructor
+        public SplashPage()
+        {
+            InitializeComponent();
+
+            Loaded += SplashPage_Loaded;
+        }
+
+        void SplashPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            VesselAB.setABListener(new MyCallbacks());
+        }
+
+        class MyCallbacks : ABListener
+        {
+            public void testLoading()
+            {
+            }
+
+            public void testLoaded(String testName, TestVariation variation)
+            {
+                (Application.Current.RootVisual as PhoneApplicationFrame).Navigate(new Uri("/HomePage.xaml", UriKind.Relative));
+            }
+
+            public void testNotAvailable(TestVariation variation)
+            {
+                (Application.Current.RootVisual as PhoneApplicationFrame).Navigate(new Uri("/HomePage.xaml", UriKind.Relative));
+            }
+        };
+
+    }
+}
